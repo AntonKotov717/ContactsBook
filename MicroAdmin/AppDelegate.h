3faf3ac8e986816107dd710b415ae6388626713b
AntonKotov <AntonKotov717@outlook.com>
2016-03-18 11:43:01 +0000

    ContactsBook

---
+++ MicroAdmin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MicroAdmin
+//
+//  Created by dev on 2016. 2. 13..
+//  Copyright © 2016년 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MicroAdmin/ContactEditViewController.h
@@ -0,0 +1,42 @@
+//
+//  ContactModifyViewController.h
+//  MicroAdmin
+//
+//  Created by dev on 2/18/16.
+//  Copyright © 2016 company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactEditViewController : UIViewController<UITextFieldDelegate, NSURLConnectionDelegate>{
+    UIDatePicker *datePicker;
+    NSMutableData *mutableData;
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *txtCustomer;
+@property (weak, nonatomic) IBOutlet UITextField *txtGender;
+@property (weak, nonatomic) IBOutlet UITextField *txtBirthday;
+@property (weak, nonatomic) IBOutlet UITextField *txtFirstName;
+@property (weak, nonatomic) IBOutlet UITextField *txtLastName;
+@property (weak, nonatomic) IBOutlet UITextField *txtEmailAddr;
+@property (weak, nonatomic) IBOutlet UITextField *txtPhoneNumber;
+@property (weak, nonatomic) IBOutlet UITextField *txtAddr;
+@property (weak, nonatomic) IBOutlet UITextField *txtCity;
+@property (weak, nonatomic) IBOutlet UITextField *txtZip;
+@property (weak, nonatomic) IBOutlet UITextField *txtPostalAddr;
+@property (weak, nonatomic) IBOutlet UITextField *txtPostalZip;
+@property (weak, nonatomic) IBOutlet UITextField *txtPostalCity;
+@property (weak, nonatomic) IBOutlet UITextField *txtEnterNote;
+@property (weak, nonatomic) IBOutlet UITextField *txtCompany;
+@property (weak, nonatomic) IBOutlet UITextField *txtSelectCountry;
+
+@property (weak, nonatomic) IBOutlet UITextField *txtPostalCountry;
+
+- (IBAction)OnSaveContact:(id)sender;
+
+- (IBAction)OnDeleteContact:(id)sender;
+
+-(void) InitContactData;
+@property (strong, nonatomic) NSString *contactID;
+
+@end
